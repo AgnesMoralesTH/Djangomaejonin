ce56fb749e2460cbd7ef9c2ca4bd013f0a194003
AgnesMoralesTH <agnesmoralesth@gmail.com>
2021-05-21 22:56:46 +0700

    added views and routes

--- MAEJONIN/base/views.py
+++ MAEJONIN/base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Category, Photo
 # Create your views here.
+def home(request):
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+    context = {'categories':categories, 'photos':photos}
+    return render(request, 'base/index.html', context)
